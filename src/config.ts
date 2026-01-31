72b3a25d063460a36c4aace42440524e2267e889
Silverbone57 <165157525+Silverbone57@users.noreply.github.com>
2026-01-31 09:21:15 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Mobinu Shadhin | Future Dentist';
+export const SITE_DESCRIPTION = 'Psychology student at USF and licensed insurance professional focused on clinical excellence and patient-centered dental care.';
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
