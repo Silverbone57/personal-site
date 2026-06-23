a2a03bc251d1369e3c34c448dffdee63c53c6f99
Silverbone57 <165157525+Silverbone57@users.noreply.github.com>
2026-06-23 16:52:32 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mobinu Shadhin';
-export const SITE_DESCRIPTION = 'Aspiring Dentist and Licensed Insurance Professional.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Mobinu Shadhin | Tech Sales Professional';
+export const SITE_DESCRIPTION = 'High-output sales professional specializing in pipeline management, high-volume outreach, and full-cycle closing.';
+export const GENERATE_SLUG_FROM_TITLE = true;
